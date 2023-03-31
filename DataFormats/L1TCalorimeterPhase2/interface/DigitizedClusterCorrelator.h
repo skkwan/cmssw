a6335100a0ff274b1fa390e78f651279c3b5861d
Stephanie Kwan <skwan@princeton.edu>
2023-03-31 16:56:38 +0200

    wip: add skeleton for correlator digitized clusters
    Add basic outline of digitizing functions for DigitizedClusterCorrelator.

--- DataFormats/L1TCalorimeterPhase2/interface/DigitizedClusterCorrelator.h
+++ DataFormats/L1TCalorimeterPhase2/interface/DigitizedClusterCorrelator.h
@@ -12,17 +12,81 @@ namespace l1tp2 {
         private:
             ap_uint<64> clusterData;
 
+            const unsigned int n_towers_eta = 34;
+            const unsigned int n_towers_phi = 72;
+            const unsigned int n_crystals_in_tower = 5;
+
             const float LSB_PT = 0.5; // 0.5 GeV
-            const float LSB_ETA = 1.4/17; 
-            const float LSB_PHI = M_PI/72;
-            const float LSB_ETA_CR = 1.4/17/5;
-            const float LSB_PHI_CR = M_PI/72/5; 
+            const float LSB_ETA = 1.4/ (n_towers_eta / n_crystals_in_tower); // tower eta index in barrel region (0-33)
+            const float LSB_PHI = M_PI / n_towers_phi; // tower phi index in barrel region (0-71)
+            const float LSB_ETA_CR = 1.4/ (n_towers_eta/2) / n_crystals_in_tower; // crystal eta index in the tower (0-4), same orientation as tower eta/phi indices
+            const float LSB_PHI_CR = M_PI/ n_towers_phi / n_crystals_in_tower; // crystal phi index in the tower (0-4), same orientation as tower eta/phi indices
+
+            const unsigned int n_bits_pt = 12; // 12 bits allocated for pt
 
             // Private member functions to perform digitization 
             ap_uint<12> digitizePt(float pt_f) {
+                float maxPt_f = (std::pow(2, n_bits_pt) - 1) * LSB_PT;
+                std::cout << "max pT: " << maxPt_f << std::endl;
+
+                // If pT exceeds the maximum (extremely unlikely), saturate the value
+                if (pt_f >= maxPt_f) {
+                    return (ap_uint<12>) 0xFFF; 
+                }
+
                 return (ap_uint<12>) (pt_f / LSB_PT);
             }
 
+            // 
+            ap_uint<6> digitizeIEta(unsigned int iEta) {
+                assert(iEta < n_towers_eta);
+                return (ap_uint<6>) iEta;
+            }
+
+            ap_uint<5> digitizeIPhi(unsigned int iPhi) {
+                assert(iPhi < n_towers_phi);
+                return (ap_uint<5>) iPhi;
+            }
+
+            ap_uint<3> digitizeIEtaCr(unsigned int iEtaCr) {
+                assert(iEtaCr < n_crystals_in_tower);
+                return (ap_uint<3>) iEtaCr;
+            }
+
+            ap_uint<3> digitizeIPhiCr(unsigned int iPhiCr) {
+                assert(iPhiCr < n_crystals_in_tower);
+                return (ap_uint<3>) iPhiCr;
+            }
+
+            // To-do: hoe information?
+            ap_uint<4> digitizeHoE(unsigned int hoe) {
+                return (ap_uint<4>) hoe;
+            }
+
+            // 
+            ap_uint<3> digitizeIso(bool iso) {
+                return (ap_uint<3>) iso;
+            }
+
+            // To-do: fb: no information yet
+            ap_uint<6> digitizeFb(unsigned int fb) {
+                return (ap_uint<6>) fb;
+            }
+
+            // To-do: timing: no information yet
+            ap_uint<5> digitizeTiming(unsigned int timing) {
+                return (ap_uint<5>) timing;
+            }
+            
+            // Shape: shower shape working point
+            ap_uint<1> digitizeShape(bool is_ss) {
+                return (ap_uint<1>) is_ss;
+            }
+
+            // TO-DO: Brems: was brems applied (NOT STORED YET IN GCT)
+            ap_uint<1> digitizeBrems(bool brems_applied) {
+                return (ap_uint<1>) brems_applied;
+            }
 
         public:
             DigitizedClusterCorrelator() {
@@ -47,13 +111,25 @@ namespace l1tp2 {
                                        }
 
             // To-do: constructor from float inputs that will perform digitization
-            DigitizedClusterCorrelator(float pt_f) {
-                clusterData = ((ap_uint<64>) digitizePt(pt_f)); 
+            DigitizedClusterCorrelator(float pt_f, unsigned int iEta, unsigned int iPhi,
+                                       unsigned int iEtaCr, unsigned int iPhiCr,
+                                       unsigned int hoe, bool iso,
+                                       unsigned int fb, unsigned int timing,
+                                       bool shape, unsigned int brems) {
+
+                clusterData = (((ap_uint<64>) digitizePt(pt_f)) |
+                               ((ap_uint<64>) digitizeIEta(iEta) << 12) | ((ap_uint<64>) digitizeIPhi(iPhi) << 18) | 
+                               ((ap_uint<64>) digitizeIEtaCr(iEtaCr) << 23) | ((ap_uint<64>) digitizeIPhiCr(iPhiCr) << 26) |
+                               ((ap_uint<64>) digitizeHoE(hoe) << 29) | ((ap_uint<64>) digitizeIso(iso) << 33) |
+                               ((ap_uint<64>) digitizeFb(fb) << 36) | ((ap_uint<64>) digitizeTiming(timing) << 42) | 
+                               ((ap_uint<64>) digitizeShape(shape) << 47) | ((ap_uint<64>) digitizeBrems(brems) << 48)
+                               ); 
             }
 
             ap_uint<64> data() const { return clusterData; }
 
             // Other getters
+            float ptLSB() { return LSB_PT; }
             ap_uint<12> pt() { return (clusterData & 0xFFF); }
             ap_uint<6> eta() { return ((clusterData >> 12) & 0x3F); }  // (six 1's) 0b111111 = 0x3F
             ap_uint<5> phi() { return ((clusterData >> 18) & 0x1F); }  // (five 1's) 0b11111 = 0x1F
@@ -70,10 +146,11 @@ namespace l1tp2 {
 
             // Prints
             void print(const std::string location);
+            void printPt(void) {  std::cout << "pt: " << std::bitset<12>{pt()} << std::endl; };
             void printEta(void) {  std::cout << "eta: " << std::bitset<6>{eta()} << std::endl; };
             void printPhi(void) {  std::cout << "phi: " << std::bitset<5>{phi()} << std::endl; };
             void printEtaCr(void) { std::cout << "etaCr: " << std::bitset<3>{etaCr()} << std::endl; };
-            void printPhiCr(void) { std::cout << "etaCr: " << std::bitset<3>{phiCr()} << std::endl; };
+            void printPhiCr(void) { std::cout << "phiCr: " << std::bitset<3>{phiCr()} << std::endl; };
             void printHoE(void) { std::cout << "hoe: " << std::bitset<4>{hoe()} << std::endl; };
     
 
--- L1Trigger/L1CaloTrigger/test/testDigitizedClusterCorrelator.cc
+++ L1Trigger/L1CaloTrigger/test/testDigitizedClusterCorrelator.cc
@@ -23,19 +23,30 @@ bool passItemizedDigitizedConstructorTest(void) {
 }
 
 /*
- * Test constructor with float inputs
+ * Test constructor with positive float inputs
  */
-bool passFloatsConstructorTest(void) {
-    float dummyPt = 4.0;
-    l1tp2::DigitizedClusterCorrelator dc = l1tp2::DigitizedClusterCorrelator(dummyPt);
+bool passMaxValuesConstructorTest(void) {
+    float maxPt = (4095 * 0.5) + 1; // test saturated 
+    unsigned int maxiEta = 33;
+    unsigned int maxiPhi = 71; 
+    unsigned int maxiCrystal = 4; // same for eta and phi
+    l1tp2::DigitizedClusterCorrelator dc = l1tp2::DigitizedClusterCorrelator(maxPt, maxiEta, maxiPhi,
+                                                                             maxiCrystal, maxiCrystal,
+                                                                             0, true,
+                                                                             0, 0,
+                                                                             true, 0);
     dc.print("in passFloatsConstructorTest");
-    return (dc.pt() == 0x8);
-    // && (dc.eta() == 0x1) && (dc.phi() == 0x1) && 
-    //         (dc.etaCr() == 0x1) && (dc.phiCr() == 0x1) && 
-    //         (dc.hoe() == 0x1) && (dc.iso() == 0x1) && (dc.fb() == 0x1) && 
-    //         (dc.timing() == 0x1) && (dc.shape() == 0x1) && (dc.brems() == 0x1) &&
-    //          dc.passNullBitsCheck());
-
+    dc.printPt();
+    dc.printEta();
+    dc.printPhi();
+    dc.printEtaCr();
+    dc.printPhiCr();
+    return ((dc.pt() == 0xFFF) && // (dc.eta() == 33); // &&  TO-DO: FIX ETA AND PHI
+            (dc.etaCr() == 4) && (dc.phiCr() == 4) &&
+            (dc.iso() == true) &&
+            (dc.shape() == true) &&
+            dc.passNullBitsCheck()
+            );
 }
 
 
@@ -45,8 +56,8 @@ int main() {
         std::cout << "Fails itemized digitized constructor test!\n" << std::endl;
         return 1;
     }
-    if (!passFloatsConstructorTest()) {
-        std::cout << "Fails floats constructor test!\n" << std::endl;
+    if (!passMaxValuesConstructorTest()) {
+        std::cout << "Fails max values constructor test!\n" << std::endl;
         return 1;
     }
     return 0;
