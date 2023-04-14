e1f8145f4dcc4a5bd98e08067afcf48594e7b71e
Stephanie Kwan <skwan@princeton.edu>
2023-04-14 15:18:09 +0200

    update digitized cluster GT unit tests for ap_int eta/phi

--- L1Trigger/L1CaloTrigger/test/testDigitizedClusterGT.cc
+++ L1Trigger/L1CaloTrigger/test/testDigitizedClusterGT.cc
@@ -15,8 +15,8 @@ int passItemizedDigitizedConstructorTest(void) {
   bool isFullyDigitizedInputs = true;
   ap_uint<1> isValid = 0x1;
   ap_uint<16> pt = 0x1;
-  ap_uint<13> phi = 0x1;
-  ap_uint<14> eta = 0x1;
+  ap_int<13> phi = 0x1;
+  ap_int<14> eta = 0x1;
   l1tp2::DigitizedClusterGT cGT = l1tp2::DigitizedClusterGT(isValid, pt, phi, eta, isFullyDigitizedInputs);
   return (cGT.passNullBitsCheck() && (cGT.pt() == 0x1) && (cGT.phi() == 0x1) && (cGT.eta() == 0x1));
 }
@@ -27,12 +27,11 @@ int passItemizedDigitizedConstructorTest(void) {
 int passPositiveConstructorTest(void) {
   bool isValid = true;
   float pt_f = 2048;     // max value is 2047.97
-  float phi_f = M_PI;    // phi = +180 should be 0b 1 1111 1111 1110 (LSB is the sign)
-  float eta_f = 1.4841;  // becomes 1.4841 / (pi/ (2^12 - 1)) = 1934.49, truncates to 1934 = 0x78E = 0b11110001110
-  // (contd.) finally bit shift left by 1, with the LSB = 0 since this is positive, which gives 0b111100011100 = 0xF1C
+  float phi_f = M_PI;    // +180 degrees should be 0b 1111 1111 1111 
+  float eta_f = 1.4841;  // should be 1.4841 / (pi/ (2^12 - 1)) = 1934.49, truncates to 1934 = 0x78E = 0b11110001110
 
   l1tp2::DigitizedClusterGT cGT = l1tp2::DigitizedClusterGT(isValid, pt_f, phi_f, eta_f);
-  return ((cGT.pt() == 0xFFFF) && (cGT.phi() == 0x1FFE) && (cGT.eta() == 0xF1C) && cGT.passNullBitsCheck());
+  return ((cGT.pt() == 0xFFFF)) && (cGT.phi() == 0xFFF) && (cGT.eta() == 0x78E) && cGT.passNullBitsCheck();
 }
 
 /*
@@ -41,12 +40,11 @@ int passPositiveConstructorTest(void) {
 int passNegativeConstructorTest(void) {
   bool isValid = true;
   float pt_f = 2048;        // max value is 2047.97
-  float phi_f = -1 * M_PI;  // phi = -180 should be 0b 1 1111 1111 1111 (LSB is the sign) = 0x1FFF
-  float eta_f = -1.4841;    // becomes 1.4841 / (pi/ (2^12 - 1)) = 1934.49, truncates to 1934 = 0x78E = 0b11110001110
-  // (contd.) finally bit shift left by 1, with the LSB = 1 since this is positive, which gives 0b111100011101 = 0xF1D
+  float phi_f = -1 * M_PI;  // should be negative of the positive test result
+  float eta_f = -1.4841;    // should be negative of the positive test result
 
   l1tp2::DigitizedClusterGT cGT = l1tp2::DigitizedClusterGT(isValid, pt_f, phi_f, eta_f);
-  return ((cGT.pt() == 0xFFFF) && (cGT.phi() == 0x1FFF) && (cGT.eta() == 0xF1D) && cGT.passNullBitsCheck());
+  return (cGT.pt() == 0xFFFF) && (cGT.phi() == (-1) * 0xFFF) && (cGT.eta() == (-1) * 0x78E) && cGT.passNullBitsCheck();
 }
 
 /*
@@ -60,6 +58,7 @@ int passSignedIntegerRepr(void) {
   float precision = 0.01;
 
   l1tp2::DigitizedClusterGT cGT = l1tp2::DigitizedClusterGT(isValid, pt_f, phi_f, eta_f);
+
   return (round_to(cGT.realEta(), precision) == round_to(eta_f, precision)) &&
          (round_to(cGT.realPhi(), precision) == round_to(phi_f, precision));
 }
