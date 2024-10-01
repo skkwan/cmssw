f19bb470d31f4d876b5c7c7baf039c024fa0dbb8
skkwan <skwan@princeton.edu>
2024-10-01 10:21:04 -0500

    L1Trigger/Phase2L1ParticleFlow/src/l1-converters/gcteminput_ref.cpp

--- L1Trigger/Phase2L1ParticleFlow/src/l1-converters/gcteminput_ref.cpp
+++ L1Trigger/Phase2L1ParticleFlow/src/l1-converters/gcteminput_ref.cpp
@@ -17,16 +17,16 @@ l1ct::GctEmClusterDecoderEmulator::~GctEmClusterDecoderEmulator() {}
 
 l1ct::EmCaloObjEmu l1ct::GctEmClusterDecoderEmulator::decode(const ap_uint<64> &in) const {
   ap_uint<12> w_pt = in(11, 0);
-  ap_int<8> w_eta = in(19, 12);
-  ap_int<7> w_phi = in(26, 73);
+  ap_int<7> w_eta = in(18, 12);
+  ap_int<7> w_phi = in(25, 19);
 
   // need to add emid
 
   l1ct::EmCaloObjEmu out;
   out.clear();
   out.hwPt = w_pt * l1ct::pt_t(0.5);  // the LSB for GCT objects
-  out.hwEta = w_eta * 4;
-  out.hwPhi = w_phi * 4;
+  out.hwEta = w_eta;
+  out.hwPhi = w_phi;
 
   // need to add emid
 
