734a41472dc094b5eaeb53598f4a2f2c36a08d1e
Stephanie Kwan <skwan@princeton.edu>
2025-02-06 17:39:11 -0600

    add module to apply baseline selections to e, mu, and jets

---
+++ PhysicsTools/NanoAODTools/python/postprocessing/examples/higgsinoSkimModule.py
@@ -0,0 +1,85 @@
+# This is an example of a NanoAODTools Module to add one variable to nanoAODs.
+# Note that:
+# -the new variable will be available for use in the subsequent modules
+# -it is possible to update the value for existing variables
+#
+# Example of using from command line:
+# nano_postproc.py outDir /eos/cms/store/user/andrey/f.root -I PhysicsTools.NanoAODTools.postprocessing.examples.exampleModule exampleModuleConstr
+#
+# Example of running in a python script: see test/example_postproc.py
+#
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+
+class exampleProducer(Module):
+    def __init__(self, jetSelection, muoSelection, eleSelection):
+        self.jetSel = jetSelection
+        self.muoSel = muoSelection
+        self.eleSel = eleSelection
+        pass
+
+    def beginJob(self):
+        pass
+
+    def endJob(self):
+        pass
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("EventMass", "F")
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+
+        electrons = Collection(event, "Electron")
+        muons = Collection(event, "Muon")
+        jets = Collection(event, "Jet")
+
+        # reminder: currently there are no isolation selections here, can change this
+        # might be time-consuming to convert this to a list
+        nElectronsPassing = sum(1 for e in filter(self.eleSel, electrons))
+        nMuonsPassing = sum(1 for m in filter(self.muoSel, muons))
+        nJetsPassing = sum(1 for j in filter(self.jetSel, jets))
+
+        print(f"{nElectronsPassing}, {nMuonsPassing}, {nJetsPassing}")
+
+        # In the full processor we will do more advanced checks like cleaning the jets from the electrons and muons,
+        # but at the minimum we need at least two jets
+        return ((nJetsPassing >= 2) and ((nElectronsPassing >= 2) or (nMuonsPassing >= 2)))
+
+        # return ((len(muons) >= 2) or (len(electrons) >= 2))
+
+        # Remainder of the example, which also computed a branch EventMass
+        # electrons = Collection(event, "Electron")
+        # muons = Collection(event, "Muon")
+        # jets = Collection(event, "Jet")
+        # eventSum = ROOT.TLorentzVector()
+        # for lep in muons:
+        #     eventSum += lep.p4()
+        # for lep in electrons:
+        #     eventSum += lep.p4()
+        # for j in filter(self.jetSel, jets):
+        #     eventSum += j.p4()
+
+        # self.out.fillBranch("EventMass", eventSum.M())
+
+        # return True
+
+
+# define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
+
+higgsinoSkimModule = lambda: exampleProducer(jetSelection=lambda j: j.pt > 25,
+                                             # muons: identical to sel_ms_mask except no cut on muon isolation: muon_iso="miniisotight", in case we want to anti-iso muons later?
+                                             muoSelection=lambda m: (m.pt > 20) and m.mediumId and (abs(m.eta) < 2.5) and (abs(m.ip3d) < 0.1) and (abs(m.dz) < 0.2),
+                                             # electrons: use 0.10 max ip3d (it's max 0.05 in the barrel and max 0.10 in the endcap)
+                                             #            use 0.2 max dz (it's max 0.1 in the barrel and max 0.2 in the endcap)
+                                             #            No isolation for now?
+                                             eleSelection=lambda e: (e.pt > 20) and (abs(e.eta) < 2.5) and (abs(e.ip3d) < 0.10) and (abs(e.dz) < 0.2))
+
--- PhysicsTools/NanoAODTools/test/example_postproc.py
+++ PhysicsTools/NanoAODTools/test/example_postproc.py
@@ -4,6 +4,7 @@
 # adding a new variable using a Module.
 #
 from PhysicsTools.NanoAODTools.postprocessing.examples.exampleModule import *
+from PhysicsTools.NanoAODTools.postprocessing.examples.higgsinoSkimModule import *
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from importlib import import_module
@@ -12,14 +13,29 @@ import sys
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-fnames = ["root://cms-xrd-global.cern.ch///store/mc/RunIISummer20UL18NanoAODv9/DY1JetsToLL_M-50_MatchEWPDG20_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/120000/13374A29-B61F-7443-AF78-C0C04D479595.root"]
+# DY1Jets
+# /DY1JetsToLL_M-50_MatchEWPDG20_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM
+# fnames = ["root://cms-xrd-global.cern.ch///store/mc/RunIISummer20UL18NanoAODv9/DY1JetsToLL_M-50_MatchEWPDG20_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/120000/13374A29-B61F-7443-AF78-C0C04D479595.root"]
+
+
+# TTTo2L2Nu:
+# 2018: /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM
+# fnames = []
+# 2017: /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM
+
+# Signal:
+# /SMS-TChiZH-mNLSP200To1500_mLSP0To600_TuneCP5_13TeV-madgraphMLM-pythia8RunIISummer20UL18NanoAODv9FSUL18_FSUL18_106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM
+# fnames = []
+
+# Data
+fnames = ["root://cms-xrd-global.cern.ch///store/data/Run2018A/DoubleMuon/NANOAOD/UL2018_MiniAODv2_NanoAODv9-v1/270000/C489C20E-FD93-8B42-9F63-0AB2FB0F5C39.root"]
 
 p = PostProcessor(outputDir=".",
                   inputFiles=fnames,
-                  cut="((nMuon >= 2) || (nElectron >= 2))",
-                  modules=[exampleModuleConstr()],
+                  cut="((nMuon >= 2) || (nElectron >= 2)) && (nJet >= 2)",
+                  modules=[higgsinoSkimModule()],
                   provenance=True,
-                  maxEntries=10, #just read the first maxEntries events
+                  maxEntries=1000, #just read the first maxEntries events
                   )
 p.run()
 
